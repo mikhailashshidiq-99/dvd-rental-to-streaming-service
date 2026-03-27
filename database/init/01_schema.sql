3500caee0646c9d19120600953bb9c86c1bf90eb
Mikhail Arumbinang <mikhailashshidiqmaulanaarumbinang@mail.ugm.ac.id>
2026-03-27 09:02:45 +0700

    feat(query): added create table streaming_sessions query

---
+++ database/init/01_schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS daily_device_telemetry_summary CASCADE;
+DROP TABLE IF EXISTS streaming_sessions CASCADE;
+DROP TABLE IF EXISTS devices CASCADE;
+
+CREATE TABLE devices (
+    device_id UUID PRIMARY KEY,
+    device_type VARCHAR(50) NOT NULL,
+    os VARCHAR(50) NOT NULL,
+    last_online TIMESTAMP
+);
+
+CREATE TABLE streaming_sessions (
+    session_id UUID PRIMARY KEY,
+    device_id UUID,
+    profile UUID,
+    content_id INT,
+    start_timestamp TIMESTAMP,
+    end_timestamp TIMESTAMP,
+    average_bitrate INT,
+    is_offline_playback BOOLEAN,
+    experiment_variant_id VARCHAR(50),
+    CONSTRAINT fk_device
+        FOREIGN KEY (device_id)
+        REFERENCES devices(device_id)
+);
